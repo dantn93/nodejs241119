856219ce7b6b97b657a3239e8b2e1796bb77abd8
Tran Ngoc Dan <dantn93@gmail.com>
2019-11-24 19:03:25 +0700

    task3

--- hw2/task-2/index.js
+++ hw2/task-2/index.js
@@ -1,29 +1,32 @@
 let fs = require("fs");
 let formatDistance = require("date-fns/formatDistance");
+let format = require("date-fns/format")
 let viLocale = require("date-fns/locale/vi");
+let XLSX = require('xlsx');
 let obj = [];
 
 // format number
 function formatNumber(num) {
   return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
 }
-// format index
-function formatIndex(num, size) {
-  var s = num + "";
-  while (s.length < size) s = "0" + s;
-  return s;
-}
+
 // Async
-fs.readFile("./products.json", function(err, data) {
+fs.readFile("./../products.json", function(err, data) {
   if (err === null) {
     obj = JSON.parse(data);
     let numberOfProduct = 0;
     obj.forEach((value, index) => {
+      //Task 2
+      // Print the list to the console with following template for each product:
+      // ${name} - ${price}VND - Cập nhật cách đây: ${fromNow}
+      // Format the price with comma (,) as thousand separators. (Google for a snippet)
+      // Use date-fns formatDistance to convert dateUpdated to fromNow with Vietnamese locale
+
       numberOfProduct += 1;
       value.dateUpdated = new Date(value.dateUpdated);
       let fromNow = formatDistance(new Date(), value.dateUpdated, {locale: viLocale});
-      console.log(`${formatIndex(index, 4)} - ${value.name} - ${formatNumber(value.price)}VND - Cập nhật cách đây ${fromNow}`);
+      console.log(`${value.id} - ${value.name} - ${formatNumber(value.price)}VND - Cập nhật cách đây ${fromNow}`);
     });
-    console.log("\n\nNumber of products: ", numberOfProduct);
+    console.log("\nNumber of products: %d\n", numberOfProduct);
   }
 });
---
+++ hw2/task-3/index.js
@@ -0,0 +1,52 @@
+let fs = require("fs");
+let formatDistance = require("date-fns/formatDistance");
+let format = require("date-fns/format")
+let viLocale = require("date-fns/locale/vi");
+let XLSX = require('xlsx');
+let obj = [];
+
+// Async
+fs.readFile("./../products.json", function(err, data) {
+  if (err === null) {
+    obj = JSON.parse(data);
+    obj.forEach((value) => {
+      value.dateUpdated = new Date(value.dateUpdated);
+
+      //Task 3
+      //Create new field updated from dateUpdated with following format: MM/DD/YYYY (use date-fns)
+      value.updated =  format(value.dateUpdated, "MM/dd/yyyy")
+
+      // Delete dateUpdated field (we don't want to generate this column later to Excel)
+      delete value.dateUpdated
+      
+    });
+    
+    //Task 3
+    // Use xlsx library to convert products.json to a Microsoft Excel file buffer
+    // Write the buffer to hard drive as products.xlsx and should be able to open in Excel.
+
+    
+    // create 'worksheet' object from json
+    const ws = XLSX.utils.json_to_sheet(obj);
+    
+    // Optional: config columns width (character length)
+    ws['!cols'] = [{ width: 20 }, { width: 15 }, { width: 20 }, { width: 20 }, { width: 20 }];
+    
+    
+    // create 'workbook' object (which contains multiple sheet)
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Products');
+    
+    // convert to Microsoft EXCEL workbook and write to a Buffer object
+    const buf = XLSX.write(wb, { type: 'buffer', bookType: 'xlsx' });
+
+    // write file hard drive
+    fs.writeFile('./products.xlsx', buf, (err) => {
+      if(err === null) {
+        console.log("Created file products.xlsx")
+      } else {
+        console.log(err)
+      }
+    })
+  }
+});
